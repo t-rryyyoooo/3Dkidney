3c14349c04461c14ff69634b8f412992a74e99c7
Tanimoto Ryo <rryyyoooo@i.softbank.jp>
2020-02-03 17:38:42 +0900

    Finish to make Unet

--- Unet.py
+++ Unet.py
@@ -2,40 +2,31 @@ import tensorflow as tf
 
 def CreateConv3DBlock(x, filters, n = 2, use_bn = True, apply_pooling = True, name = 'convblock'):
     for i in range(n):
-        x = tf.keras.layers.ZeroPadding3D(padding=(1, 1, 0))(x)
-        x = tf.keras.layers.Conv3D(filters[i], (3,3,3), padding='valid', name=name+'_conv'+str(i+1))(x)
+        x = tf.compat.v1.keras.layers.Conv3D(filters[i], (3,3,3), padding='same', name=name+'_conv'+str(i+1))(x)
         if use_bn:
-            x = tf.keras.layers.BatchNormalization(name=name+'_BN'+str(i+1))(x)
-        x = tf.keras.layers.Activation('relu', name=name+'_relu'+str(i+1))(x)
-
+            x = tf.compat.v1.keras.layers.BatchNormalization(name=name+'_BN'+str(i+1))(x)
+        x = tf.compat.v1.keras.layers.Activation('relu', name=name+'_relu'+str(i+1))(x)
+    
     convresult = x
 
     if apply_pooling:
-        x = tf.keras.layers.MaxPool3D(pool_size=(2,2,2), strides=(1, 1, 1), name=name+'_pooling')(x)
+        x = tf.compat.v1.keras.layers.MaxPool3D(pool_size=(2,2,2), name=name+'_pooling')(x)
 
     return x, convresult
 
 
 def CreateUpConv3DBlock(x, contractpart, filters, n = 2, use_bn = True, name = 'upconvblock'):
     # upconv x
-    x = tf.keras.layers.Conv3DTranspose((int)(x.shape[-1]), (2,2,2), strides=(2,2,2), padding='same', use_bias = False, name=name+'_upconv')(x)
-    # concatenate contractpart and x
-    c = [(i-j)//2 for (i, j) in zip(contractpart[0].shape[1:4].as_list(), x.shape[1:4].as_list())]
-    contract_crop = tf.keras.layers.Cropping3D(cropping=((c[0],c[0]),(c[1],c[1]),(c[2],c[2])))(contractpart[0])
-    if len(contractpart) > 1:
-        crop1 = tf.keras.layers.Cropping3D(cropping=((c[0],c[0]),(c[1],c[1]),(c[2],c[2])))(contractpart[1])
-        #crop2 = tf.keras.layers.Cropping3D(cropping=((c[0],c[0]),(c[1],c[1]),(c[2],c[2])))(contractpart[2])
-        #x = tf.keras.layers.concatenate([contract_crop, crop1, crop2, x])
-        x = tf.keras.layers.concatenate([contract_crop, crop1, x])
-    else:
-        x = tf.keras.layers.concatenate([contract_crop, x])
+    x = tf.compat.v1.keras.layers.Conv3DTranspose((int)(x.shape[-1]), (2,2,2), strides=(2,2,2), padding='same', use_bias = False, name=name+'_upconv')(x)
+
+    x = tf.compat.v1.keras.layers.concatenate([contractpart, x])
 
     # conv x 2 times
     for i in range(n):
-        x = tf.keras.layers.Conv3D(filters[i], (3,3,3), padding='valid', name=name+'_conv'+str(i+1))(x)
+        x = tf.compat.v1.keras.layers.Conv3D(filters[i], (3,3,3), padding='same', name=name+'_conv'+str(i+1))(x)
         if use_bn:
-            x = tf.keras.layers.BatchNormalization(name=name+'_BN'+str(i+1))(x)
-        x = tf.keras.layers.Activation('relu', name=name+'_relu'+str(i+1))(x)
+            x = tf.compat.v1.keras.layers.BatchNormalization(name=name+'_BN'+str(i+1))(x)
+        x = tf.compat.v1.keras.layers.Activation('relu', name=name+'_relu'+str(i+1))(x)
 
     return x
 
@@ -54,30 +45,32 @@ def Construct3DUnetModel(input_images, nclasses, use_bn = True, use_dropout = Tr
 
     with tf.name_scope("dropout"):
         if use_dropout:
-            x = tf.keras.layers.Dropout(0.5, name='dropout')(x)
+            x = tf.compat.v1.keras.layers.Dropout(0.5, name='dropout')(x)
 
     with tf.name_scope("expand3"):
-        x = CreateUpConv3DBlock(x, [contract3], (256, 256), n = 2, use_bn = use_bn, name = 'expand3')
+        x = CreateUpConv3DBlock(x, contract3, (256, 256), n = 2, use_bn = use_bn, name = 'expand3')
 
     with tf.name_scope("expand2"):
-        x = CreateUpConv3DBlock(x, [contract2], (128, 128), n = 2, use_bn = use_bn, name = 'expand2')
+        x = CreateUpConv3DBlock(x, contract2, (128, 128), n = 2, use_bn = use_bn, name = 'expand2')
 
     with tf.name_scope("expand1"):
-        x = CreateUpConv3DBlock(x, [contract1], (64, 64), n = 2, use_bn = use_bn, name = 'expand1')
+        x = CreateUpConv3DBlock(x, contract1, (64, 64), n = 2, use_bn = use_bn, name = 'expand1')
 
     with tf.name_scope("segmentation"):
         layername = 'segmentation_{}classes'.format(nclasses)
-        x = tf.keras.layers.Conv3D(nclasses, (1,1,1), activation='softmax', padding='same', name=layername)(x)
+        x = tf.compat.v1.keras.layers.Conv3D(nclasses, (1,1,1), activation='softmax', padding='same', name=layername)(x)
 
     return x
 
 
 import numpy as np
 
-inputshape=(100, 100, 100, 1)
-inputs = tf.keras.layers.Input(shape=inputshape, name="input")
+inputshape=(128, 128, 16, 1)
+inputs = tf.compat.v1.keras.layers.Input(shape=inputshape, name="input")
 segmentation = Construct3DUnetModel(inputs, 3)
 
-model = tf.keras.models.Model(inputs, segmentation,name="3DUnet")
+model = tf.compat.v1.keras.models.Model(inputs, segmentation,name="3DUnet")
 model.summary()
+
+
 
